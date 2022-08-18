1091fbf70bf6e7db404792b731e7367d27e8c88f
patxi <patxiarana05@gmail.com>
2022-08-18 15:04:29 -0300

    adding users to the seeder

--- seeders/20201109013159-create-demo-user.js
+++ seeders/20201109013159-create-demo-user.js
@@ -12,15 +12,34 @@ module.exports = {
       image: 'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
       createdAt: new Date,
       updatedAt: new Date
-    }], {});
+    },{
+      firstName: 'Usuario2',
+      lastName: 'Demouser',
+      email: 'test2@test.com',
+      // Important: Password not encrypted yet! 
+      password: '1234',
+      roleId: 1,
+      image: 'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
+      createdAt: new Date,
+      updatedAt: new Date
+},{
+
+  firstName: 'Usuario3',
+  lastName: 'Demouser',
+  email: 'test3@test.com',
+  // Important: Password not encrypted yet! 
+  password: '1234',
+  roleId: 1,
+  image: 'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
+  createdAt: new Date,
+  updatedAt: new Date
+}
+  ], {});
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+
+      await queryInterface.bulkDelete('Users', null, {});
+     
   }
 };
