028d8b43f20e103ff06e80d8848577edc5bf8ab1
patxi <patxiarana05@gmail.com>
2022-08-19 11:55:02 -0300

    slide model and migration created

--- seeders/20201109013159-create-demo-user.js
+++ seeders/20201109013159-create-demo-user.js
@@ -12,28 +12,7 @@ module.exports = {
       image: 'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
       createdAt: new Date,
       updatedAt: new Date
-    },{
-      firstName: 'Usuario2',
-      lastName: 'Demouser',
-      email: 'test2@test.com',
-      // Important: Password not encrypted yet! 
-      password: '1234',
-      roleId: 1,
-      image: 'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
-      createdAt: new Date,
-      updatedAt: new Date
-},{
-
-  firstName: 'Usuario3',
-  lastName: 'Demouser',
-  email: 'test3@test.com',
-  // Important: Password not encrypted yet! 
-  password: '1234',
-  roleId: 1,
-  image: 'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
-  createdAt: new Date,
-  updatedAt: new Date
-}
+    }
   ], {});
   },
 
