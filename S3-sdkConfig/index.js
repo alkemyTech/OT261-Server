174d112c20588f1681ffad65cf9f929f8cd24c1d
florMignini <flormignini29@gmail.com>
2022-08-29 11:28:32 -0300

    reestructured files

--- S3-sdkConfig/index.js
+++ S3-sdkConfig/index.js
@@ -1,22 +1,14 @@
-const express = require("express");
 const dotenv = require("dotenv");
 dotenv.config();
 // package to handle 'multipart/form-data' content-type
 const multer = require("multer");
 //S3 client for aws-sdk
-const { S3Client } = require("@aws-sdk/client-s3");
+const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
 
 // access variables
 const bucketName = process.env.BUCKET_NAME;
 const bucketPublic = process.env.BUCKET_PUBLIC_KEY;
 const bucketSecret = process.env.BUCKET_SECRET_KEY;
-
-const S3 = new S3Client({
-  credentials: {
-    accessKeyId: bucketPublic,
-    secretAccessKey: bucketSecret,
-  },
-});
 
 // The memory storage engine stores the files in memory as Buffer objects
 // memoryStorage Object
@@ -25,4 +17,20 @@ const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
 //config to upload a single image --input name image name-- middleware to use in post routes
-upload.single("image");
+// upload.single("image"); middleware for uploading files
+
+exports.s3Upload = async (file) => {
+  const s3Client = new S3Client({
+    credentials: {
+      accessKeyId: bucketPublic,
+      secretAccessKey: bucketSecret,
+    },
+  });
+
+  const params = {
+    Bucket: bucketName,
+    Key: file.originalname,
+    Body: file.buffer,
+  };
+  return s3Client.send(new PutObjectCommand(params));
+};
