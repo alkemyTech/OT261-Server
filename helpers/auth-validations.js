31e3efe522600c3eeb8a8099f75a801f5912c3da
jruizsilva <ruizsilvajonathan@gmail.com>
2022-09-15 13:22:44 -0300

    minor changes

--- helpers/auth-validations.js
+++ helpers/auth-validations.js
@@ -4,8 +4,11 @@ const { User } = db.sequelize.models
 const existsUserWithThisEmail = async email => {
   const existsUser = await User.findOne({ where: { email } })
   console.log('USEEER', existsUser)
-  if (!existsUser && existsUser.status) {
+  if (!existsUser) {
     throw new Error(`El correo o la contraseña no son válidos`)
+  }
+  if (!existsUser.status) {
+    throw new Error(`Usuario eliminado`)
   }
   return true
 }
