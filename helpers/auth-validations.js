36a0a2cc57c49a19b3c755f0528e294d3f6a010e
jruizsilva <ruizsilvajonathan@gmail.com>
2022-09-15 13:15:25 -0300

    user status validation in  existsUserWithThisEmail middleware

--- helpers/auth-validations.js
+++ helpers/auth-validations.js
@@ -4,7 +4,7 @@ const { User } = db.sequelize.models
 const existsUserWithThisEmail = async email => {
   const existsUser = await User.findOne({ where: { email } })
   console.log('USEEER', existsUser)
-  if (!existsUser) {
+  if (!existsUser && existsUser.status) {
     throw new Error(`El correo o la contraseña no son válidos`)
   }
   return true
