f7c60dccf36d8a866df83cc0d7eba09570020c13
florMignini <flormignini29@gmail.com>
2022-09-19 17:43:37 -0300

    activities seeder creation

--- seeders/20220824183003-create-activities.js
+++ seeders/20220824183003-create-activities.js
@@ -1,23 +1,40 @@
-'use strict';
+"use strict";
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkInsert('Activities', [{
-      name: 'DemoUser',
-      image: 'https://images.pexels.com/photos/177598/pexels-photo-177598.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-      content: 'activities',
-      createdAt: new Date,
-      updatedAt: new Date
-    }], {});
+    await queryInterface.bulkInsert(
+      "Activities",
+      [
+        {
+          name: "DemoUser1",
+          image:
+            "https://images.pexels.com/photos/177598/pexels-photo-177598.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+          content: "activities1",
+        },
+        {
+          name: "DemoUser2",
+          image:
+            "https://images.pexels.com/photos/177598/pexels-photo-177598.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+          content: "activities2",
+        },
+        {
+          name: "DemoUser3",
+          image:
+            "https://images.pexels.com/photos/177598/pexels-photo-177598.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+          content: "activities3",
+        },
+        {
+          name: "DemoUser4",
+          image:
+            "https://images.pexels.com/photos/177598/pexels-photo-177598.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+          content: "activities4",
+        },
+      ],
+      {}
+    );
   },
 
-
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
-  }
+    await queryInterface.bulkDelete("Activities", null, {});
+  },
 };
